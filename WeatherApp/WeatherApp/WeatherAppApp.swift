0b4c62479ff050943dab19781fc952a22231e954
Merve Uslu Erbas <83906167+merveusluerbas@users.noreply.github.com>
2024-07-27 16:33:55 +0200

    Create weather app project for testing

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Merve Uslu Erbas on 27/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
